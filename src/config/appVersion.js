df3022e9c3ebe8c5ba4ac47accf52d9e2244ab65
ralbertini2 <raphael.albertini@gmail.com>
2026-06-15 18:17:33 -0300

    chore: prepare v4 footer

--- src/config/appVersion.js
+++ src/config/appVersion.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = 'v3.0.12';
+export const APP_VERSION = 'v4.0.0-pre';
 export const APP_NAME = 'Playback Cifras Beta';
 export const APP_VERSION_LABEL = `${APP_NAME} | ${APP_VERSION}`;
