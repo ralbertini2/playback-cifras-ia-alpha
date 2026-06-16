6cf5077624e87506908c1bc288bbbd9ad1485ccf
ralbertini2 <raphael.albertini@gmail.com>
2026-06-15 22:05:45 -0300

    feat: add stage scroll

--- src/config/appVersion.js
+++ src/config/appVersion.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = 'v4.0.8';
+export const APP_VERSION = 'v4.0.9';
 export const APP_NAME = 'Playback Cifras Beta';
 export const APP_VERSION_LABEL = `${APP_NAME} | ${APP_VERSION}`;
